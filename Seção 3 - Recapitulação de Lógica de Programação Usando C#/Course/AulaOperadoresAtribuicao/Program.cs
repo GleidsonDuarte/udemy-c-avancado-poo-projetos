c6b6b016cbb0565777963bf5dff07330d0e85c43
GleidsonTeixeira <gleidson.duarte@connectcorp.com.br>
2019-02-11 17:52:16 -0200

    Seção 3: Realizadas as aulas de saída de dados, operadores de aritméticos e de atribuição.

---
+++ Seção 3 - Recapitulação de Lógica de Programação Usando C#/Course/AulaOperadoresAtribuicao/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AulaOperadoresAtribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+
+            int b = 10;
+            b++;
+            Console.WriteLine(b);
+
+            b--;
+            Console.WriteLine(b);
+
+            int c = 10;
+
+            // Primeiro passa o valor de c para b e depois incrementa o valor de c
+            int d = c++;
+            // Saída: c = 11, b = 10
+
+            /*
+             * Primeiro incrementa o valor de c e depois passa o valor do mesmo para b
+             * int d = c++;
+             * Saída: c = 11, b = 11
+            */
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+}
+}
+}
