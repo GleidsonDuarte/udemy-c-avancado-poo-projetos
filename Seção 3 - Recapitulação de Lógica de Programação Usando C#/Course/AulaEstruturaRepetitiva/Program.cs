ae774b6940442bcc08ccc0b854ea0ff6c3ce674a
GleidsonTeixeira <gleidson.duarte@connectcorp.com.br>
2019-02-15 18:20:58 -0200

    Finalizando aulas da seção 3.

---
+++ Seção 3 - Recapitulação de Lógica de Programação Usando C#/Course/AulaEstruturaRepetitiva/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace AulaEstruturaRepetitiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Enquanto for verdadeiro faça algo, se for falso, saia
+            while (true)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+
+                Console.WriteLine("Digite outro número: ");    
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
