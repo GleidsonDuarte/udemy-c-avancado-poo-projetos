b360b0332ff657e5a67e29dcc2a42866538b91c8
Gleidson Duarte <work.gleidsonduarte@hotmail.com>
2019-02-17 18:03:38 -0300

    Iniciando aulas da seção 4.

--- Seção 3 - Recapitulação de Lógica de Programação Usando C#/Course/AulaEstruturaRepetitiva/Program.cs
+++ Seção 3 - Recapitulação de Lógica de Programação Usando C#/Course/AulaEstruturaRepetitiva/Program.cs
@@ -7,18 +7,43 @@ namespace AulaEstruturaRepetitiva
     {
         static void Main(string[] args)
         {
+            For();
+            While();
+        }
+
+        public static void For()
+        {
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+
+            Console.WriteLine("Soma = " + soma);
+        }
+
+        public static void While()
+        {
             Console.WriteLine("Digite um número: ");
             double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             // Enquanto for verdadeiro faça algo, se for falso, saia
-            while (true)
+            while (x >= 0.0)
             {
                 double raiz = Math.Sqrt(x);
                 Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
 
-                Console.WriteLine("Digite outro número: ");    
+                Console.WriteLine("Digite outro número: ");
                 x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             }
+
+            Console.WriteLine("Número negativo!");
         }
     }
 }
