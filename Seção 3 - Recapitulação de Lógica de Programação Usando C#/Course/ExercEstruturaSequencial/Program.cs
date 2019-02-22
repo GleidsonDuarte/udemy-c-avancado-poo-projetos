6d8bc5df6e839d2179a041cbfdc80b357124a3e9
GleidsonTeixeira <gleidson.duarte@connectcorp.com.br>
2019-02-22 17:47:08 -0300

    Resolvendo os exerc´´icios da seção 3.

--- Seção 3 - Recapitulação de Lógica de Programação Usando C#/Course/ExercEstruturaSequencial/Program.cs
+++ Seção 3 - Recapitulação de Lógica de Programação Usando C#/Course/ExercEstruturaSequencial/Program.cs
@@ -10,7 +10,8 @@ namespace ExercEstruturaSequencial
             
         }
 
-        // Exercício: Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números.
+        #region Exercício 1
+        // Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números.
         public static void SomaDoisNumeros()
         {
             Console.WriteLine("Digite o primeiro número!");
@@ -23,7 +24,9 @@ namespace ExercEstruturaSequencial
 
             Console.WriteLine($"SOMA = {soma}");
         }
+        #endregion
 
+        #region Exercício 2
         /*
          * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo
          * com quatro casas decimais conforme exemplos.
@@ -37,12 +40,15 @@ namespace ExercEstruturaSequencial
             double area = Math.Pow(raio, 2) * Math.PI;
             Console.WriteLine($"A = {area.ToString("F4", CultureInfo.InvariantCulture)}");
         }
+        #endregion
 
+        #region Exercício 3
         /*
          * Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
          * de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
          */
-         public static void diferencaEntreABeCD()
+
+        public static void diferencaEntreABeCD()
         {
             Console.WriteLine("Digite o valor de A");
             int a = int.Parse(Console.ReadLine());
@@ -59,11 +65,31 @@ namespace ExercEstruturaSequencial
             int diferença = a * b - c * d;
             Console.WriteLine($"Diferença = {diferença}");
         }
+        #endregion
 
+        #region Exercício 4
         /*
          * Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
-         * hora e calcula o salário desse funcionário.A seguir, mostre o número e o salário do funcionário, com duas
+         * hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas
          * casas decimais.
          */
+
+        public static void CalculaSalario()
+        {
+            Console.WriteLine("Digite o número do funcionário!");
+            int numeroFuncionario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o número de horas trabalhadas pelo funcionário!");
+            int numeroHorasTrabalhadas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da hora trabalhada do funcionário!");
+            double horasTrabalhadas = double.Parse(Console.ReadLine());
+
+            double salario = numeroHorasTrabalhadas * horasTrabalhadas;
+
+            Console.WriteLine($"NUMBER = {numeroFuncionario}");
+            Console.WriteLine($"SALARY = U$ {salario.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        #endregion
     }
 }
