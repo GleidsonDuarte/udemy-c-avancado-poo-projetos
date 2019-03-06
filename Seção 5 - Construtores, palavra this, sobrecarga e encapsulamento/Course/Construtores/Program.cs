bddd23ed756ff4d4a873c66360d7c0a320f03427
Gleidson Duarte <work.gleidsonduarte@hotmail.com>
2019-03-05 21:43:10 -0300

    Concluídas as aulas da seção 5.

---
+++ Seção 5 - Construtores, palavra this, sobrecarga e encapsulamento/Course/Construtores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 20);
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+    }
+}
