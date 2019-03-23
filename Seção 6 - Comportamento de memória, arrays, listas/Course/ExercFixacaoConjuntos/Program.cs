9f851eefd05cc9e70a79eee78038fcf892150df2
GleidsonTeixeira <gleidson.duarte@connectcorp.com.br>
2019-03-23 12:02:22 -0300

    Resolvidos os exercícios e concluídas todas as aulas da seção 6.

---
+++ Seção 6 - Comportamento de memória, arrays, listas/Course/ExercFixacaoConjuntos/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercFixacaoConjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O curso A possui quantos alunos?");
+            int cursoA = int.Parse(Console.ReadLine());
+
+            HashSet<int> conjuntoA = new HashSet<int>();
+
+            for (int i = 0; i < cursoA; i++)
+            {
+                Console.WriteLine($"Digite o número do {i + 1}º aluno!");
+                int alunoA = int.Parse(Console.ReadLine());
+
+                conjuntoA.Add(alunoA);
+            }
+
+            Console.WriteLine();
+
+            HashSet<int> conjuntoB = new HashSet<int>();
+
+            Console.WriteLine("O curso B possui quantos alunos?");
+            int cursoB = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cursoB; i++)
+            {
+                Console.WriteLine($"Digite o número do {i + 1}º aluno!");
+                int alunoB = int.Parse(Console.ReadLine());
+
+                conjuntoB.Add(alunoB);
+            }
+
+            Console.WriteLine();
+
+            HashSet<int> conjuntoC = new HashSet<int>();
+
+            Console.WriteLine("O curso C possui quantos alunos?");
+            int cursoC = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cursoC; i++)
+            {
+                Console.WriteLine($"Digite o número do {i + 1}º aluno!");
+                int alunoC = int.Parse(Console.ReadLine());
+
+                conjuntoC.Add(alunoC);
+            }
+
+            Console.WriteLine();
+
+            conjuntoA.UnionWith(conjuntoB);
+            conjuntoA.UnionWith(conjuntoC);
+
+            Console.WriteLine($"Total de alunos: {conjuntoA.Count}");
+        }
+    }
+}
