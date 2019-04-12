9066e0fc73f19d570ef0636c7b141830df6e3735
GleidsonTeixeira <gleidson.duarte@connectcorp.com.br>
2019-04-12 18:21:39 -0300

    Continuação das aulas e exercícios da seção 9.

---
+++ Seção 9 - Enumerações e composição/Course/Composicao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Composicao.Entities;
+using Composicao.Entities.Enums;
+
+namespace Composicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o nome do departamento: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Insira as informações do trabalhador: ");
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+            Console.Write("Insira o level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Insira o valor do salário: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+
+        }
+    }
+}
---
+++ Seção 9 - Enumerações e composição/Course/Enum1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Enum1.Entities;
+using Enum1.Entities.Enums;
+
+namespace Enum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+             
+            Console.WriteLine(order);
+
+            string texto = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(texto);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
